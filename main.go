004965bc185e65282e3740b8083c41bd361c1529
StepanPokhodzhai <stelkerstepan@gmail.com>
2023-01-08 20:19:53 +0200

    Update main.go

--- main.go
+++ main.go
@@ -2,10 +2,28 @@ package main
 
 import "fmt"
 
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for ", color, " is ", hex)
+	}
+}
+
 func main() {
+	//var colors map[string]string
+
+	//colors := make(map[int]string)
+
 	colors := map[string]string{
 		"red":   "#ff0000",
 		"green": "#4bf745",
+		"white": "#ffffff",
 	}
-	fmt.Println(colors)
+
+	//colors[10] = "ffffff"
+
+	//delete(colors, 10)
+
+	//fmt.Println(colors)
+	printMap(colors)
+
 }
